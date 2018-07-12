ccc8cc5f9b7e2d430b7ea516b3730074682880da
Karyil Raju <Sijumon.KaryilRaju@experian.com>
2018-07-12 15:07:37 +0800

    Changes made

--- src/main/java/com/project/Accessibilty/PersonNode.java
+++ src/main/java/com/project/Accessibilty/PersonNode.java
@@ -4,6 +4,8 @@ import com.project.person.PersonImpl;
 import com.project.person.PersonIndex;
 import com.project.relationship.RelationshipType;
 
+import java.util.List;
+
 public class PersonNode {
 
     public void createPersontree(PersonIndex personIndex) {
@@ -11,8 +13,56 @@ public class PersonNode {
 
     }
 
-    public PersonImpl searchRelationshipPerson(RelationshipType type){
+    public List<PersonImpl> searchBrothers(PersonImpl person){
 
+        return null;
+    }
+
+    public List<PersonImpl> searchSisters(PersonImpl person){
+        return  null;
+    }
+
+    public PersonImpl searchFather(PersonImpl person){
+        return null;
+    }
+
+    public PersonImpl searchMother(PersonImpl person){
+        return null;
+    }
+
+    public List<PersonImpl> searchSons(PersonImpl person){
+        return null;
+    }
+
+    public List<PersonImpl> searchDaughters(PersonImpl person){
+        return null;
+    }
+
+    public List<PersonImpl> searchCousins(PersonImpl person){
+        return null;
+    }
+
+    public PersonImpl searchGrandFather(PersonImpl person){
+        return null;
+    }
+
+    public PersonImpl searchGrandMother(PersonImpl person){
+        return  null;
+    }
+
+    public List<PersonImpl> searchGrandSons(PersonImpl person){
+        return null;
+    }
+
+    public List<PersonImpl> searchGrandDaughters(PersonImpl person){
+        return null;
+    }
+
+    public List<PersonImpl> searchAunts(PersonImpl person){
+        return null;
+    }
+
+    public List<PersonImpl> searchUncles(PersonImpl person){
         return null;
     }
 }
---
+++ src/main/java/com/project/acceptor/Interactive.java
@@ -0,0 +1,4 @@
+package com.project.acceptor;
+
+public class Interactive {
+}
