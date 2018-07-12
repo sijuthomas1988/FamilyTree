c62e813265007318d0daad2634168959e14e19af
sijumon.skr <sijuthomas1988@gmail.com>
2018-07-12 13:13:59 +0800

    Temp Changes

---
+++ src/main/java/com/project/Accessibilty/PersonNode.java
@@ -0,0 +1,18 @@
+package com.project.Accessibilty;
+
+import com.project.person.PersonImpl;
+import com.project.person.PersonIndex;
+import com.project.relationship.RelationshipType;
+
+public class PersonNode {
+
+    public void createPersontree(PersonIndex personIndex) {
+
+
+    }
+
+    public PersonImpl searchRelationshipPerson(RelationshipType type){
+
+        return null;
+    }
+}
---
+++ src/main/java/com/project/relationship/RelationshipImpl.java
@@ -0,0 +1,6 @@
+package com.project.relationship;
+
+public class RelationshipImpl {
+
+
+}
